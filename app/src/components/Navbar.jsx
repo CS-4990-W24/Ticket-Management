31057af8f36efdf19f9818e9075edac6dab25ac6
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2024-01-06 15:46:24 -0800

    feat: navbar and MUI install

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,18 +1,186 @@
 import { NavLink } from "react-router-dom";
 import "@/styles/navbar.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+    AppBar,
+    Container,
+    Toolbar,
+    Typography,
+    Box,
+    IconButton,
+    MenuItem,
+    Menu,
+    Button,
+    Tooltip,
+    Avatar,
+} from "@mui/material";
+import { useState } from "react";
+
+const pages = [
+    <NavLink to="/"> Home</NavLink>,
+    <NavLink to="/upload"> Ticket Upload</NavLink>,
+    <NavLink to="/admin"> Admin</NavLink>,
+    <NavLink to="/checkout"> Checkout</NavLink>
+];
+const settings = ["Account", "Logout"];
 
 function Navbar() {
+    const [anchorElNav, setAnchorElNav] = useState(null);
+    const [anchorElUser, setAnchorElUser] = useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
     return (
-        <nav>
-            <NavLink to="/"> Home</NavLink>
-            <br />
-            <NavLink to="/upload"> Ticket Upload</NavLink>
-            <br />
-            <NavLink to="/admin"> Admin</NavLink>
-            <br />
-            <NavLink to="/checkout"> Checkout</NavLink>
-        </nav>
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        sx={{
+                            mr: 2,
+                            display: { xs: "none", md: "flex" },
+                            fontWeight: 700,
+                            color: "inherit",
+                            textDecoration: "none",
+                        }}
+                    >
+                        Ticket Management
+                    </Typography>
+
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "flex", md: "none" },
+                        }}
+                    >
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: "bottom",
+                                horizontal: "left",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "left",
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: "block", md: "none" },
+                            }}
+                        >
+                            {pages.map((page, index) => (
+                                <MenuItem
+                                    key={index}
+                                    onClick={handleCloseNavMenu}
+                                >
+                                    {page}
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        sx={{
+                            mr: 2,
+                            display: { xs: "flex", md: "none" },
+                            flexGrow: 1,
+                            fontFamily: "monospace",
+                            fontWeight: 700,
+                            letterSpacing: ".3rem",
+                            color: "inherit",
+                            textDecoration: "none",
+                        }}
+                    >
+                        Ticket Management
+                    </Typography>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "none", md: "flex" },
+                        }}
+                    >
+                        {pages.map((page, index) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: "white", display: "block" }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton
+                                onClick={handleOpenUserMenu}
+                                sx={{ p: 0 }}
+                            >
+                                <Avatar
+                                    alt="Guest"
+                                    src="/static/images/avatar/2.jpg"
+                                />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: "45px" }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: "top",
+                                horizontal: "right",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "right",
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem
+                                    key={setting}
+                                    onClick={handleCloseUserMenu}
+                                >
+                                    <Typography textAlign="center">
+                                        {setting}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 }
-
 export default Navbar;
