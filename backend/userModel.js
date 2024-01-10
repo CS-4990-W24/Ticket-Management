4644043b3b70641cc0607967139c4d4a1b9b0673
HanmoZhang0909 <zhanghanmo888@gmail.com>
2024-01-10 11:45:39 -0800

    added email existance varification

--- backend/userModel.js
+++ backend/userModel.js
@@ -1,6 +1,20 @@
 const connection = require('./database');
 
 const createUser = (userData, callback) => {
+
+    const checkEmailSql = 'SELECT * FROM Users WHERE Email = ?';
+
+    connection.query(checkEmailSql, [userData.Email], (error, results) => {
+        if (error) {
+            console.error('Error in query:', error);
+            return callback(error);
+        }
+        if (results.length > 0) {
+            console.log('Email already exists');
+            return callback(new Error('Email already exists'));
+        }
+        else {
+
     const sql = 'INSERT INTO Users (Email, Password, UserRole) VALUES (?, ?, ?)';
     const values = [userData.Email, userData.Password, userData.UserRole];
 
@@ -12,7 +26,9 @@ const createUser = (userData, callback) => {
         console.log('Query results:', results);
         callback(null, results);
     });
-};
+}
+});
+}
 
 
 module.exports = { createUser };
