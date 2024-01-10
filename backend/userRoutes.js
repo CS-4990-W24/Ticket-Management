e8bce8deefcee4852a450a9169deb6ddfe8106e3
HanmoZhang0909 <zhanghanmo888@gmail.com>
2024-01-10 12:07:25 -0800

    separated userRoutes' duty to userController.

--- backend/userRoutes.js
+++ backend/userRoutes.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const userModel = require('./userModel');
+const userController = require('./userController');
 
-router.post('/users', (req, res) => {
-    userModel.createUser(req.body, (error, results) => {
-        if (error) throw error;
-        res.status(201).send(`User added with ID: ${results.insertId}`);
-    });
-});
+router.post('/users', userController.createUserController);
 
 module.exports = router;
