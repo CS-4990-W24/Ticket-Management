9501011c723a41f520959191925ee60b898ca9a8
HanmoZhang0909 <zhanghanmo888@gmail.com>
2024-01-10 12:35:40 -0800

    update project structure

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,4 @@
-const connection = require('./database');
+const connection = require('../database');
 
 const checkEmailExists = (email, callback) => {
     const sql = 'SELECT * FROM Users WHERE Email = ?';
