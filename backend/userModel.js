3deb38b2e3771c63408210f2329e5e78a9425b8e
HanmoZhang0909 <zhanghanmo888@gmail.com>
2024-01-10 12:11:07 -0800

    simplified userModel functions.

--- backend/userModel.js
+++ backend/userModel.js
@@ -1,34 +1,14 @@
 const connection = require('./database');
 
-const createUser = (userData, callback) => {
+const checkEmailExists = (email, callback) => {
+    const sql = 'SELECT * FROM Users WHERE Email = ?';
+    connection.query(sql, [email], callback);
+};
 
-    const checkEmailSql = 'SELECT * FROM Users WHERE Email = ?';
-
-    connection.query(checkEmailSql, [userData.Email], (error, results) => {
-        if (error) {
-            console.error('Error in query:', error);
-            return callback(error);
-        }
-        if (results.length > 0) {
-            console.log('Email already exists');
-            return callback(new Error('Email already exists'));
-        }
-        else {
-
+const insertUser = (userData, callback) => {
     const sql = 'INSERT INTO Users (Email, Password, UserRole) VALUES (?, ?, ?)';
     const values = [userData.Email, userData.Password, userData.UserRole];
+    connection.query(sql, values, callback);
+};
 
-    connection.query(sql, values, (error, results) => {
-        if (error) {
-            console.error('Error in query:', error);
-            return callback(error);
-        }
-        console.log('Query results:', results);
-        callback(null, results);
-    });
-}
-});
-}
-
-
-module.exports = { createUser };
+module.exports = { checkEmailExists, insertUser };
