f6692415838e90a28ec9b3bfb09d7a4311bb1670
joshhkim02 <joshhkim02@gmail.com>
2024-01-15 19:03:11 -0700

    Small format change

--- app/src/pages/UploadTicket.jsx
+++ app/src/pages/UploadTicket.jsx
@@ -7,8 +7,6 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
 
-
-
 export default function UploadTicket() {
     const [Price, setPrice] = useState('');
     const [Seat, setSeat] = useState('');
