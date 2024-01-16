2553745ebbc2f30a91229e78c60f7354b379f0ed
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2024-01-16 15:31:25 -0800

    chore: merge conflict baggage

--- backend/routes/orderRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const {deleteTicketController} = require('../controllers/orderController');
-const router = express.Router();
-
-router.delete('/tickets/:id', deleteTicketController);
-
-module.exports = router;    
