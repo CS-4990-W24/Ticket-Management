5bc555b3e5b0b992f3abca222558e4b78ebeaae9
Josh Kim <97993478+joshhkim02@users.noreply.github.com>
2024-01-15 19:02:30 -0700

    Connected checkout page to backend (#6)
    * Created error page and info tooltip
    
    * Added backend code for checkout page
    
    * Saving changes for later
    
    * Saved checkout changes for later
    
    * Finished checkout page functionality
    
    * Deleted vite dependency from root

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ticketRoutes = require('./routes/ticketRoutes');
+const orderRoutes = require('./routes/orderRoutes')
 const app = express();
 const port = 3000;
 const cors = require('cors');
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', ticketRoutes);
+app.use('/api', orderRoutes);
+
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
